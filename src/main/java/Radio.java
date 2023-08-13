e0da85280f62d73d598a0d319644f8e402bd5a36
Roman <borisenko0893@gmail.com>
2023-08-13 19:40:58 +0300

    third commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,71 @@
+
+public class Radio {
+
+    //РАДИО-СТАНЦИИ
+    public int currentStation;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {  //выбор станции
+        if (newCurrentStation > 9) {
+            newCurrentStation = 0;
+        }
+
+
+        currentStation = newCurrentStation;
+    }
+
+    public void nextRadioStation() {   //переключение станции вперед
+        int next = currentStation + 1;
+
+        currentStation = next;
+        if (next > 9) {
+            currentStation = 0;
+        }
+
+    }
+
+    public void prevRadioStation() {  //переключение станции назад
+        int prev = currentStation - 1;
+
+        currentStation = prev;
+        if (prev < 0) {
+            currentStation = 9;
+        }
+    }
+
+    public void setToMaxStation() {
+        currentStation = 9;
+    }
+
+    // ГРОМКОСТЬ РАДИО
+    public int increaseVolume;
+
+    public int getIncreaseVolume() {
+        return increaseVolume;
+    }
+
+    public void setIncreaseVolume(int newIncreaseVolume) {  //выбор громкости
+        if (newIncreaseVolume > 100) {
+            newIncreaseVolume = 100;
+        }
+        if (newIncreaseVolume < 0) {
+            newIncreaseVolume = 0;
+        }
+
+
+        increaseVolume = newIncreaseVolume;
+    }
+
+    public void setMaxVolume() {
+        increaseVolume = 100;
+    }
+
+}
+
+
+
+
+
